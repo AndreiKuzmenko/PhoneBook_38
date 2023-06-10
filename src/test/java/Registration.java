4482d5c2b71260f0a8a76fa054de11026eb91902
AndreiKuzmenko <eccehomo28@gmail.com>
2023-06-10 09:15:23 +0300

    09062023_study

---
+++ src/test/java/Registration.java
@@ -0,0 +1,47 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class Registration {
+
+    WebDriver wd;
+
+    @BeforeMethod
+    public void init(){
+        wd = new ChromeDriver();
+        wd.navigate().to("https://telranedu.web.app/home");
+        wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+    }
+    @Test
+    public void RegPositive(){
+        wd.findElement(By.xpath("//*[.='LOGIN']")).click();
+
+        int i = (int)(System.currentTimeMillis()/1000)%3600;
+
+        WebElement emailInput = wd.findElement(By.xpath("//input[1]"));
+        emailInput.click();
+        emailInput.clear();
+        emailInput.sendKeys("ref" +i + "@gmail.com");
+
+        WebElement passInput = wd.findElement(By.xpath("//input[2]"));
+        passInput.click();
+        passInput.clear();
+        passInput.sendKeys("$Qwe1234");
+
+        wd.findElement(By.xpath("//button[2]")).click();
+        Assert.assertTrue(wd.findElements(By.xpath("//button")).size() > 0);
+    }
+
+ @AfterMethod
+public void tearDown(){
+
+}
+}
