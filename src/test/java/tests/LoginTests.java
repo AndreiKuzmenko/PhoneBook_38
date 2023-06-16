53cbbe082ad0ef11f13529ba85080b0ca69f2951
AndreiKuzmenko <eccehomo28@gmail.com>
2023-06-16 12:28:07 +0300

    16062023_study

---
+++ src/test/java/tests/LoginTests.java
@@ -0,0 +1,89 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LoginTests extends TestBase{
+
+//    WebDriver wd;
+//
+//@BeforeMethod
+//public void init(){
+//    wd = new ChromeDriver();
+//    wd.navigate().to("https://telranedu.web.app/home");
+//    wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+//
+//}
+@BeforeMethod
+public void precondition() {
+   if(app.getUser().isLogged()){
+       app.getUser().logout();
+   }
+}
+
+//    @Test
+//    public void loginPositiveTest() {
+//        wd.findElement(By.xpath("//*[.='LOGIN']")).click();
+//        WebElement emailInput = wd.findElement(By.xpath("//input[1]"));
+//        emailInput.click();
+//        emailInput.clear();
+//        emailInput.sendKeys("ref@gmail.com");
+//
+//        WebElement passInput = wd.findElement(By.xpath("//input[2]"));
+//        passInput.click();
+//        passInput.clear();
+//        passInput.sendKeys("$Qwe1234");
+//
+//        wd.findElement(By.xpath("//button[1]")).click();
+//        pause(3000);
+//        Assert.assertTrue(wd.findElements(By.xpath("//button")).size() > 0);
+//
+//    }
+    @Test
+    public void loginPositiveTestBase() {
+
+      String email = "ref@gmail.com" ,password = "$Qwe1234";
+        app.getUser(). openLoginForm();
+        app.getUser(). fillLoginForm(email, password);
+        app.getUser(). submitLogin();
+        app.getUser(). pause(3000);
+       // Assert.assertTrue(wd.findElements(By.xpath("//button")).size() > 0);
+        Assert.assertTrue(app.getUser().isElementPresent(By.xpath("//button")));
+    }
+//    @Test
+//    public void loginNegTestWrongEmail(){
+//        wd.findElement(By.xpath("//*[.='LOGIN']")).click();
+//        WebElement emailInput = wd.findElement(By.xpath("//input[1]"));
+//        emailInput.click();
+//        emailInput.clear();
+//        emailInput.sendKeys("refgmail.com");
+//
+//        WebElement passInput = wd.findElement(By.xpath("//input[2]"));
+//        passInput.click();
+//        passInput.clear();
+//        passInput.sendKeys("$Qwe1234");
+//
+//        wd.findElement(By.xpath("//button[1]")).click();
+//
+//
+//    }
+//    @Test
+//    public void loginNegTestWrongPassword() {
+//
+//        String email = "ref@gmail.com" ,password = "$we1234";
+//        openLoginForm();
+//        fillLoginForm(email,password);
+//        submitLogin();
+//
+//
+//
+//    }
+    @AfterMethod
+    public void tearDown(){
+
+    }
+}
