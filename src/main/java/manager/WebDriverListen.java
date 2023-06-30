43e9bf0cb84a283db46215ccecdd33ff3ae6c378
AndreiKuzmenko <eccehomo28@gmail.com>
2023-06-30 11:30:27 +0300

    28062023_study

---
+++ src/main/java/manager/WebDriverListen.java
@@ -0,0 +1,16 @@
+package manager;
+
+import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WebDriverListen extends AbstractWebDriverEventListener {
+    Logger logger = Logger.getLogger(WebDriverListen.class);
+
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
+
+    public WebDriverListener (){
+
+    }
+
+}
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
@@ -13,6 +15,7 @@ import org.testng.annotations.BeforeSuite;
 import java.util.concurrent.TimeUnit;
 
 public class TestBase {
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     static ApplicationManager app = new ApplicationManager();
 
